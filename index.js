7f916ba60642f4c01b454b5c2fbc8538ecfc4cbf
Mousse3 <tagrohn@student.uef.fi>
2021-03-30 11:39:42 +0300

    port -> PORT

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const generateId = (max) => {
     return Math.floor(Math.random() * Math.floor(max))
 }
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
